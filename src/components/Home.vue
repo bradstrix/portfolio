2217fb6e4d0bad794526a255c6c44858414a5508
DESKTOP-ERSMFIT\Buttonsz <buttonsz.dan@gmail.com>
2018-01-03 22:59:35 +0100

    Added all the starting plate.

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div>Brad Stricker Portfolio</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
